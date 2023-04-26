3454dbd4afa33dfa7b995922b259c84e30d30aaf
Dina Polousova <dinapolousova@gmail.com>
2023-04-26 17:00:23 +0300

    yandex map

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -6,6 +6,11 @@ function childhood_styles() {
 	wp_enqueue_style( 'childhood-style', get_stylesheet_uri() );
 	//wp_enqueue_style( 'header-style', get_template_directory_uri(  ) . '/assets/styles/main.min.css' ); //подключение дополнительных файлов стилей
 	// wp_enqueue_style( 'animate-style', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' ); //подключение дополнительных стилей через CDN
+
+	//переподключить jQuery
+	//wp_deregister_script( 'jquery');
+	//wp_deregister_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js');
+	//wp_enqueue_script( 'jquery');
 };
 
 function childhood_scripts() {
