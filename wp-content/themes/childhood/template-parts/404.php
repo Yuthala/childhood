38531d091fedef7bcb730a1d5080c7a23d968bca
Dina Polousova <dinapolousova@gmail.com>
2023-05-02 15:56:12 +0300

    page 404, template page

---
+++ wp-content/themes/childhood/template-parts/404.php
@@ -0,0 +1,15 @@
+<?php
+	get_header();
+?> 
+
+<div class="container toys">
+	<h2 class="subtitle">
+		<a href='<?php echo get_home_url(); ?>'>
+			такой страницы не существует
+		</a>
+	</h2>
+</div>
+
+<?php
+	get_footer();
+?> 
